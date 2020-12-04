acd6e19690a6170e8d6ad35d0c4d54570043785b
Martin Dhenu <martin.dhenu@chefkoch.de>
2020-12-04 15:09:50 +0100

    Feature: create test service

--- src/Controller/DevController.php
+++ src/Controller/DevController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
+use App\Service\Calculator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DevController extends AbstractController
 {
+    /** @var Calculator */
+    private $calculator;
+
+    /**
+     * DevController constructor.
+     * @param Calculator $calculator
+     */
+    public function __construct(Calculator $calculator)
+    {
+        $this->calculator = $calculator;
+    }
+
     /**
      * @Route("/", name="dev")
      */
     public function index(): Response
     {
+        $total = $this->calculator->add(2, 10);
+
         return $this->render('dev/index.html.twig', [
-            'controller_name' => 'DevController',
+            'total' => $total,
+            'user' => 'John Doe'
         ]);
     }
 }
---
+++ src/Service/Calculator.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Service;
+
+
+class Calculator
+{
+    public function add(int $a, int $b)
+    {
+        return $a + $b;
+    }
+}
