d4b32490c614d41946b59175f0a3f0134affa987
Martin Dhenu <martin.dhenu@chefkoch.de>
2020-12-07 11:06:26 +0100

    Feature: add phpunit

---
+++ tests/Controller/DevControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DevControllerTest extends WebTestCase
+{
+    private $client;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->client = static::createClient();
+    }
+
+    public function testSomething()
+    {
+        $this->client->request('GET', 'http://localhost:8080/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+}
