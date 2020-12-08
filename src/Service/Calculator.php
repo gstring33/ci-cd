6e4c9ef45e9e564c58bc95cb09d453cfc38f68ed
Martin Dhenu <martin.dhenu@chefkoch.de>
2020-12-08 16:48:53 +0100

    "Feature: add phpstan"

--- src/Service/Calculator.php
+++ src/Service/Calculator.php
@@ -4,6 +4,11 @@ namespace App\Service;
 
 class Calculator
 {
+    /**
+     * @param int $a
+     * @param int $b
+     * @return int
+     */
     public function add(int $a, int $b)
     {
         return $a + $b;
